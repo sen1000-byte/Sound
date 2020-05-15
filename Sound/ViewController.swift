806064ae4d8783c4651e035dfef46046cd2f7142
sen1000-byte <nishiwaki.chihiro@gmail.com>
2020-05-15 19:25:25 +0900

    サウンドアプリチャレンジの完成

--- Sound/ViewController.swift
+++ Sound/ViewController.swift
@@ -12,8 +12,12 @@ import AVFoundation
 class ViewController: UIViewController {
     
     @IBOutlet var drumButton: UIButton!
+    @IBOutlet var pianoButton: UIButton!
+    @IBOutlet var guitarButton: UIButton!
     
     let drumSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "drum_sound")!.data)
+    let pianoSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "piano_sound")!.data)
+    let guitarSoundPlayer = try! AVAudioPlayer(data: NSDataAsset(name: "guitar_sound")!.data)
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,16 +25,34 @@ class ViewController: UIViewController {
     }
     
     @IBAction func touchDownDrumButton() {
-        
         drumButton.setImage(UIImage(named: "drum_playing_image"),for: .normal)
-        
         drumSoundPlayer.currentTime = 0
         drumSoundPlayer.play()
     }
-    
     @IBAction func touchUpDrumButton() {
         drumButton.setImage(UIImage(named: "drum_image"), for: .normal)
     }
+    
+    
+    @IBAction func touchDownPianoButton() {
+        pianoButton.setImage(UIImage(named: "piano_playing_image"), for: .normal)
+        pianoSoundPlayer.currentTime = 0
+        pianoSoundPlayer.play()
+    }
+    @IBAction func touchUpPianoButton() {
+        pianoButton.setImage(UIImage(named: "piano_image"), for: .normal)
+    }
+    
+    @IBAction func touchDownGuitarButton() {
+        guitarButton.setImage(UIImage(named: "guitar_playing_image"), for: .normal)
+        guitarSoundPlayer.currentTime = 0
+        guitarSoundPlayer.play()
+    }
+    @IBAction func touchUpGUitarButton() {
+        guitarButton.setImage(UIImage(named: "guitar_image"), for: .normal)
+    }
+    
+    
 
 
 }
